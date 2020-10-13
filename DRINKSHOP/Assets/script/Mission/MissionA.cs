4b1bfe37d8228bd7bed25cd411900ded00df55f5
HongPing <hongping1224@gmail.com>
2020-10-13 19:36:43 +0800

    Added Mission

---
+++ DRINKSHOP/Assets/script/Mission/MissionA.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionA : BaseMission
+{
+    public override void DoEvent(string[] param)
+    {
+        Debug.Log("MissionA");
+    }
+}
---
+++ DRINKSHOP/Assets/script/TestMission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMission : MonoBehaviour
+{
+    string missionType;
+    private void OnGUI()
+    {
+        missionType = GUILayout.TextField(missionType);
+        if (GUILayout.Button("DoMission"))
+        {
+            Type t = Type.GetType(missionType);
+            BaseMission e = (BaseMission)Activator.CreateInstance(t);
+            e.DoEvent(null);
+        }
+    }
+}
