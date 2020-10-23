9ac171f67760036d20e449f2ebf8762144b4c18d
vicky910311 <vicky910311@gmail.com>
2020-10-23 17:19:24 +0800

    mission,timer revise

--- DRINKSHOP/Assets/script/MissionData/Mission.cs
+++ DRINKSHOP/Assets/script/MissionData/Mission.cs
@@ -6,14 +6,62 @@ using System;
 [Serializable]
 public class Mission 
 {
-    public string Name;
-    public string Narrate;
-    public bool isActive = true;
-    public bool isReach = false;
-    public bool isRewarded = false;
-    public MissionType Type;
-    public int NeedAmount;
-    public int Reward;
+    [SerializeField]
+    private string name;
+    public string Name 
+    { 
+        set { name = value; } 
+        get { return name; } 
+    }
+    [SerializeField]
+    private string narrate;
+    public string Narrate
+    {
+        set { narrate = value; }
+        get { return narrate; }
+    }
+    [SerializeField]
+    private bool isactive = true;
+    public bool isActive
+    {
+        set { isactive = value; }
+        get { return isactive; }
+    }
+    [SerializeField]
+    private bool isreach = false;
+    public bool isReach
+    {
+        set { isreach = value; }
+        get { return isreach; }
+    }
+    [SerializeField]
+    private bool isrewarded = false;
+    public bool isRewarded
+    {
+        set { isrewarded = value; }
+        get { return isrewarded; }
+    }
+    [SerializeField]
+    private MissionType type;
+    public MissionType Type
+    {
+        set { type = value; }
+        get { return type; }
+    }
+    [SerializeField]
+    private int needamount;
+    public int NeedAmount
+    {
+        set { needamount = value; }
+        get { return needamount; }
+    }
+    [SerializeField]
+    private int reward;
+    public int Reward
+    {
+        set { reward = value; }
+        get { return reward; }
+    }
 }
 public enum MissionType
 {
--- DRINKSHOP/Assets/script/PlayerData/TimeState.cs
+++ DRINKSHOP/Assets/script/PlayerData/TimeState.cs
@@ -6,10 +6,75 @@ using System;
 [Serializable]
 public class TimeState 
 {
-    public List<int> MakeTime;
-    public List<int> StaffUnlockTime;
-    public int DevelopTime;
-    public List<int> MakeTemp;
+    [SerializeField]
+    private List<int> maketime;
+    public void setMakeTime(int i,int t)
+    {
+        if(i < maketime.Count && i>=0)
+            maketime[i] = t;
+    }
+    public int getMakeTime(int i)
+    {
+        if (i < maketime.Count && i >= 0)
+            return maketime[i];
+        else
+            return -1;
+    }
+    [SerializeField]
+    private List<int> staffunlocktime;
+    public void setStaffUnlockTime(int i,int t)
+    {
+        if (i < staffunlocktime.Count && i >= 0)
+            staffunlocktime[i] = t;
+    }
+    public int getStaffUnlockTime(int i)
+    {
+        if (i < staffunlocktime.Count && i >= 0)
+            return staffunlocktime[i];
+        else
+            return -1;
+    }
+    [SerializeField]
+    private int developtime;
+    public int DevelopTime
+    {
+        set { developtime = value; }
+        get { return developtime; }
+    }
+    [SerializeField]
+    private List<int> maketemp;
+    public void setMakeTemp(int i, int t)
+    {
+        if (i < maketemp.Count && i >= 0)
+            maketemp[i] = t;
+    }
+    public int getMakeTemp(int i)
+    {
+        if (i < maketemp.Count && i >= 0)
+            return maketemp[i];
+        else
+            return -1;
+    }
+    [SerializeField]
+    private List<bool> staffunlocktemp;
+    public void setStaffUnlockTemp(int i, bool b)
+    {
+        if (i < staffunlocktemp.Count && i >= 0)
+            staffunlocktemp[i] = b;
+    }
+    public bool getStaffUnlockTemp(int i)
+    {
+        if (i < staffunlocktemp.Count && i >= 0)
+            return staffunlocktemp[i];
+        else
+            return false;
+    }
     public List<bool> StaffUnlockTemp;
-    public int DevelopTemp;
+    [SerializeField]
+    private int developtemp;
+    public int DevelopTemp
+    {
+        set { developtemp = value; }
+        get { return developtemp; }
+    }
 }
