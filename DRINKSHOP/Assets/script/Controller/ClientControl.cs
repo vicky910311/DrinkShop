e9ed80c52ae722708235d65cc6e51f211c5c7358
vicky910311 <vicky910311@gmail.com>
2020-11-03 18:06:06 +0800

    delete using editor

--- DRINKSHOP/Assets/script/Controller/ClientControl.cs
+++ DRINKSHOP/Assets/script/Controller/ClientControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using UnityEditorInternal;
+
 
 public class ClientControl 
 {
--- DRINKSHOP/Assets/script/Testing.cs
+++ DRINKSHOP/Assets/script/Testing.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
-using UnityEditor;
+
 
 public class Testing : MonoBehaviour
 {
