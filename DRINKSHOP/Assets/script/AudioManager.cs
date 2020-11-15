74235ac6346d1b22727d1daa277808ba478be30f
vicky910311 <vicky910311@gmail.com>
2020-11-15 16:11:15 +0800

    audiomanager

--- DRINKSHOP/Assets/script/AudioManager.cs
+++ DRINKSHOP/Assets/script/AudioManager.cs
@@ -2,17 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+[System.Serializable]
+public class Sounds 
 {
-    // Start is called before the first frame update
-    void Start()
+    public string name;
+    public AudioClip clip;
+    private AudioSource source;
+    [Range(0f,1f)]
+    public float volume = 1f;
+    [Range(0.5f, 1.5f)]
+    public float pitch = 1f;
+    public void SetSource(AudioSource _source)
     {
-        
+        source = _source;
+        source.clip = clip;
+    }
+    public void Play()
+    {
+        source.volume = volume;
+        source.pitch = pitch;
+        source.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool loop;
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager self;
+    [SerializeField]
+    Sounds[] sounds;
+    private void Awake()
     {
-        
+        if (self == null)
+        {
+            self = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (this != self)
+        {
+            Destroy(gameObject);
+        }
     }
+    
+    void Start()
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject _go = new GameObject("sounds" + i +"_" +sounds[i].name);
+            _go.transform.SetParent(this.transform);
+            sounds[i].SetSource(_go.AddComponent<AudioSource>());
+        }
+    }
+     public void PlaySound(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == _name)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+    }
+    
 }
