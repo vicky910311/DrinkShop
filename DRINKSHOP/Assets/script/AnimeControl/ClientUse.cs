eee985647665cd66872abe6544869ea47c01fa62
vicky910311 <vicky910311@gmail.com>
2020-11-18 17:15:45 +0800

    動畫加強

--- DRINKSHOP/Assets/script/AnimeControl/ClientUse.cs
+++ DRINKSHOP/Assets/script/AnimeControl/ClientUse.cs
@@ -18,6 +18,25 @@ public class ClientUse : MonoBehaviour
 
     public void haveCome()
     {
-        SellingAnime.self.SellBegin();
+        if (SellingAnime.self.havestock)
+        {
+            SellingAnime.self.SellBegin();
+        }
+        else
+        {
+            SellingAnime.self.Go();
+        }
+       
     } 
+    public void Speaknostock()
+    {
+        if (!SellingAnime.self.havestock)
+        {
+            SellingAnime.self.Speak.SetActive(true);
+        }
+    }
+    public void SpeakDone()
+    {
+        SellingAnime.self.Speak.SetActive(false);
+    }
 }
--- DRINKSHOP/Assets/script/AnimeControl/SellingAnime.cs
+++ DRINKSHOP/Assets/script/AnimeControl/SellingAnime.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SellingAnime : MonoBehaviour
 {
     public static SellingAnime self;
     public Animator DrinkAni, ClientAni, ArmAni;
     public bool havestock;
-    public int D, C;
+    public int D, C, S;
+    public GameObject Drink, Client, Staff, Speak;
     private void Awake()
     {
         self = this;
@@ -15,7 +17,9 @@ public class SellingAnime : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        S = PlayerDataManager.self.Player.FrontStaff;
+        Staff.GetComponent<SpriteRenderer>().sprite = GameDataManager.self.Staff.StaffData[S].Image;
+        Speak.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,9 +27,10 @@ public class SellingAnime : MonoBehaviour
     {
         
     }
-    public void ChangeStaff()
+    public void ChangeStaff(int i)
     {
-        
+        S = PlayerDataManager.self.Player.FrontStaff = i;
+        Staff.GetComponent<SpriteRenderer>().sprite = GameDataManager.self.Staff.StaffData[S].Image;
     }
     public void Staffgosleep()
     {
@@ -35,16 +40,19 @@ public class SellingAnime : MonoBehaviour
     {
 
     }
-    public void ChangeCandD()
-    {
-
-    }
+    
     public void Come()
     {
         ClientAni.SetTrigger("come");
+        Client.GetComponent<SpriteRenderer>().sprite = GameDataManager.self.Client.ClientData[C].Image;
+        Client.transform.GetChild(0).gameObject.SetActive(false);
+        Client.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = GameDataManager.self.Drink.DrinkData[D].Image;
+        Drink.GetComponent<SpriteRenderer>().sprite = GameDataManager.self.Drink.DrinkData[D].Image;
+        Speak.GetComponent<Text>().text = havestock ? GameDataManager.self.Drink.DrinkData[D].Name + "好囉~" : GameDataManager.self.Drink.DrinkData[D].Name + "沒貨了";
     }
     public void SellBegin()
     {
+        Speak.SetActive(true);
         DrinkAni.SetTrigger("appear");
         ArmAni.SetTrigger("appear");
     }
@@ -55,6 +63,7 @@ public class SellingAnime : MonoBehaviour
     }
     public void Go()
     {
+        Client.transform.GetChild(0).gameObject.SetActive(havestock ? true : false);
         ClientAni.SetTrigger("go");
     }
 }
