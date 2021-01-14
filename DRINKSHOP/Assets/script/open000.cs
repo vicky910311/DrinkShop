ad76522b177802f4f2b3bdef14f94278070f050b
vicky910311 <vicky910311@gmail.com>
2021-01-14 14:17:49 +0800

    標題動畫

---
+++ DRINKSHOP/Assets/script/open000.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class open000 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i <= 8; i++)
+        {
+
+            transform.GetChild(i).transform.rotation = Quaternion.Euler(0, 0, 30 * i);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        for (int i = 0; i<= 8; i++)
+        {
+            transform.GetChild(i).Rotate(0,0,3f);
+        }
+    }
+}
